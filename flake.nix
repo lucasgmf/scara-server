d45088913ad767179a8b6ff849dcffa95b163bd2
lucasgmf <lucasgalvaomfreitas@gmail.com>
2025-04-02 14:10:37 +0100

    Structure update to esp32

--- flake.nix
+++ flake.nix
@@ -8,65 +8,63 @@
       url = "github:numtide/flake-utils";
       inputs.systems.follows = "systems";
     };
-    pico-sdk = {
-      url = "github:raspberrypi/pico-sdk";
-      flake = false;
-    };
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-      ...
+    { self
+    , nixpkgs
+    , flake-utils
+    , ...
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        pname = "hello-world";
-        version = "0.0.1";
-        src = ./.;
+    # For more information about the C/C++ infrastructure in nixpkgs: https://nixos.wiki/wiki/C
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      pname = "hello-world"; #package name
+      version = "0.0.1";
+      src = ./.;
+      buildInputs = with pkgs; [
+        # add library dependencies here i.e.
+        #zlib
+        # Tipp: you can use `nix-locate foo.h` to find the package that provides a header file, see https://github.com/nix-community/nix-index
+      ];
+      nativeBuildInputs = with pkgs; [
+        # add build dependencies here
+        ## For mesonbuild:
+        #meson ninja
+        ## For cmake:
+        cmake
+        ## For autotools:
+        # autoconf-archive
+        # autoreconfHook
+        pkg-config
+        # clangd language server.
+        # Also start your IDE/editor from the shell provided by `nix develop` as the wrapped clangd from clang-tools needs environment variables set by the shell
+        clang-tools
+      ];
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        inherit buildInputs nativeBuildInputs;
 
-        picoSdkPath = pkgs.fetchFromGitHub {
-          owner = "raspberrypi";
-          repo = "pico-sdk";
-          rev = "ee68c78d0afae2b69c03ae1a72bf5cc267a2d94c";
-          sha256 = "sha256-wfvsDfzxkKImD7rFbqxD/+53WLebLuRpG91fjiLYDxY=";
-          fetchSubmodules = true;
-        };
+        # You can use NIX_CFLAGS_COMPILE to set the default CFLAGS for the shell
+        #NIX_CFLAGS_COMPILE = "-g";
+        # You can use NIX_LDFLAGS to set the default linker flags for the shell
+        #NIX_LDFLAGS = "-L${lib.getLib zstd}/lib -lzstd";
+      };
 
-        buildInputs = with pkgs; [
-          picotool
-        ];
-        nativeBuildInputs = with pkgs; [
-          cmake
-          pkg-config
-          clang-tools
-          gcc-arm-embedded
-          python3
-        ];
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          inherit buildInputs nativeBuildInputs;
-          shellHook = ''
-            export PICO_SDK_PATH="${picoSdkPath}"
-            export PICO_TINYUSB_PATH="${picoSdkPath}/lib/tinyusb"
-            echo "PICO_SDK_PATH set to $PICO_SDK_PATH"
-          '';
-        };
+      # Pinned gcc: remain on gcc10 even after `nix flake update`
+      #default = pkgs.mkShell.override { stdenv = pkgs.gcc10Stdenv; } {
+      #  inherit buildInputs nativeBuildInputs;
+      #};
 
-        packages.default = pkgs.stdenv.mkDerivation {
-          inherit
-            buildInputs
-            nativeBuildInputs
-            pname
-            version
-            src
-            ;
-        };
-      }
-    );
+      # Clang example:
+      default = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
+        inherit buildInputs nativeBuildInputs;
+      };
+
+      packages.default = pkgs.stdenv.mkDerivation {
+        inherit buildInputs nativeBuildInputs pname version src;
+      };
+    });
 }
