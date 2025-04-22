7a691c6deebd2a86afa94ace664fc4fb7676cbe0
lucasgmf <lucasgalvaomfreitas@gmail.com>
2025-04-22 18:05:22 +0100

    initial components definition

--- esp32-server/include/motor.h
+++ esp32-server/include/motor.h
@@ -11,9 +11,40 @@
 #define GPIOXDIR 16
 #define GPIOYDIR 27
 #define GPIOZDIR 14
+#define GPIOADIR 50 // WARN - Find GPIO number
 #define GPIOXSTP 26
 #define GPIOYSTP 25
 #define GPIOZSTP 17
+#define GPIOASTP 50 // WARN - Find GPIO number
+
+typedef struct {
+  int gpio;
+  int value; // TODO: idk?
+} motor_encoder;
+
+typedef struct {
+  bool gpiodir;
+  bool gpiostp;
+  int angle_deg; // TODO: Maybe float?
+  motor_encoder *encoder;
+} motor;
+
+typedef struct {
+  int gpio;
+  bool value;
+} micro_switch;
+
+typedef struct {
+  motor *motor_claw;
+  micro_switch *switch_claw;
+} claw;
+
+typedef struct { // TODO: Update this struct
+  int gpio;
+} colision_detector;
+
+typedef struct {
+} scara;
 
 void led_test(void);
 void motor_test(int gpiodir, int gpiostp);
