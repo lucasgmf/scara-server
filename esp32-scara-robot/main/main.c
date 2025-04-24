b920dae390ea123940541b4a05da414f13207995
lucasgmf <lucasgalvaomfreitas@gmail.com>
2025-04-24 14:58:44 +0100

    now compiler does not conflict with some random hal library

--- esp32-scara-robot/main/main.c
+++ esp32-scara-robot/main/main.c
@@ -3,6 +3,6 @@
 void motor_x_task(void *params) { motor_test(GPIOXDIR, GPIOXSTP); }
 void motor_y_task(void *params) { motor_test(GPIOYDIR, GPIOYSTP); }
 void app_main(void) {
-  xTaskCreate(motor_x_task, "Motor_x aka task1", 4096, NULL, 5, NULL);
-  xTaskCreate(motor_y_task, "Motor_x aka task2", 4096, NULL, 5, NULL);
+  /* xTaskCreate(motor_x_task, "Motor_x aka task1", 4096, NULL, 5, NULL); */
+  /* xTaskCreate(motor_y_task, "Motor_x aka task2", 4096, NULL, 5, NULL); */
 }
