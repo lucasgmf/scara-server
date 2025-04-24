2f556563a179c2c7ba87a8a1df6d2e995be60fe5
lucasgmf <lucasgalvaomfreitas@gmail.com>
2025-04-24 17:04:58 +0100

    removed led tests and updated code to use motor_t*

--- esp32-scara-robot/components/hal_user/motor.c
+++ esp32-scara-robot/components/hal_user/motor.c
@@ -1,51 +1,12 @@
 #include "motor.h"
 
-void initialize_motors(void) {
-  motor motor_x, motor_y, motor_z, motor_a;
-  motor_x.gpiostp = GPIOXDIR;
-  motor_x.gpiostp = GPIOXSTP;
-  motor_y.gpiodir = GPIOYDIR;
-  motor_y.gpiostp = GPIOYSTP;
-  motor_z.gpiodir = GPIOZDIR;
-  motor_z.gpiostp = GPIOZSTP;
-  motor_a.gpiodir = GPIOADIR;
-  motor_a.gpiostp = GPIOASTP;
-}
-
-void led_test(void) {
-  gpio_reset_pin(BLINK_LED);
-  gpio_set_direction(BLINK_LED, GPIO_MODE_OUTPUT);
-
-  while (1) {
-    gpio_set_level(BLINK_LED, 1);
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-    gpio_set_level(BLINK_LED, 0);
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-  }
-}
-
-void led_test_2(void) {
-  gpio_reset_pin(BLINK_LED);
-  gpio_set_direction(BLINK_LED, GPIO_MODE_OUTPUT);
-
-  int direction = 0;
-
-  while (1) {
-    direction = !direction;
-    gpio_set_level(BLINK_LED, direction);
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-    ESP_LOGI("led_test_2", "\ndirection: %d\nvalue: %d",
-             gpio_get_level(BLINK_LED));
-  }
-}
-
-void motor_test(motor *motor_n) {
+void motor_test(motor_t *motor_n) {
   if (motor_n == NULL) {
     ESP_LOGW("motor_test", "Pointer is null\n");
     return;
   }
-  int gpiodir = motor_n->gpiodir;
-  int gpiostp = motor_n->gpiostp;
+  int gpiodir = motor_n->gpio_dir;
+  int gpiostp = motor_n->gpio_stp;
 
   gpio_reset_pin(gpiodir);
   gpio_reset_pin(gpiostp);
@@ -88,14 +49,14 @@ void motor_test(motor *motor_n) {
   }
 }
 
-void driver_calibration(motor *motor_n) {
+void driver_calibration(motor_t *motor_n) {
   if (motor_n == NULL) {
     ESP_LOGW("driver_calibration", "Pointer is null\n");
     return;
   }
 
-  int gpiodir = motor_n->gpiodir;
-  int gpiostp = motor_n->gpiostp;
+  int gpiodir = motor_n->gpio_dir;
+  int gpiostp = motor_n->gpio_stp;
 
   gpio_reset_pin(gpiodir);
   gpio_reset_pin(gpiostp);
@@ -111,3 +72,5 @@ void driver_calibration(motor *motor_n) {
 }
 
 void calibrate_motor_pos() {}
+
+void motor_update(){}
