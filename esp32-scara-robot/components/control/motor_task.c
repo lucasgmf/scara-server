f77ad8f00b690ddf5a25d3011e3e6f3fa003329d
lucasgmf <lucasgalvaomfreitas@gmail.com>
2025-04-30 18:18:53 +0100

    refactoring motor.c and tasks

--- esp32-scara-robot/components/control/motor_task.c
+++ esp32-scara-robot/components/control/motor_task.c
@@ -1,14 +1,13 @@
-
 #include "driver/gpio.h"
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "motor.h"
 
-#define MOTOR_TASK_PERIOD_MS 10 // TODO: Maybe decrese this
+#define MOTOR_TASK_PERIOD_MS 10 // TODO: Maybe decrese this to 5
 static const char *TAG = "MotorTask";
 
-void motor_task(void *arg) {
+void motor_control_task(void *arg) {
   motor_t *motor = (motor_t *)arg;
   if (motor == NULL) {
     ESP_LOGE(TAG, "parameter motor is null, aborting.");
@@ -16,28 +15,30 @@ void motor_task(void *arg) {
     return;
   }
   TickType_t last_wake_time = xTaskGetTickCount();
-  while (1) {
-    motor_update(motor);
+  while (true) {
+    // run motor adjust position or something
     vTaskDelayUntil(&last_wake_time, pdMS_TO_TICKS(MOTOR_TASK_PERIOD_MS));
   }
 }
 
-void motor_task_start(motor_t *motor, const char *task_name,
-                      UBaseType_t priority, BaseType_t core_id) {
+/* void motors_task_init(motors_t *motors) {} */
+
+void motor_task_init(motor_t *motor, const char *task_name,
+                     UBaseType_t priority, BaseType_t core_id) {
   if (motor == NULL || task_name == NULL) {
     ESP_LOGE(TAG, "Cannot start motor task: invalid arguments");
     return;
   }
 
-  BaseType_t result = xTaskCreatePinnedToCore(motor_task, task_name,
-                                              2048, // Stack size in words
-                                              (void *)motor, priority,
-                                              NULL, // No handle needed for now
-                                              core_id);
-
-  if (result != pdPASS) {
-    ESP_LOGE(TAG, "Failed to create task %s", task_name);
-  } else {
-    ESP_LOGI(TAG, "Motor task %s started on core %d", task_name, core_id);
-  }
+ /*  BaseType_t result = xTaskCreatePinnedToCore(motor_task, task_name, */
+ /*                                              2048, // Stack size in words */
+ /*                                              (void *)motor, priority, */
+ /*                                              NULL, // No handle needed for now */
+ /*                                              core_id); */
+ /**/
+ /*  if (result != pdPASS) { */
+ /*    ESP_LOGE(TAG, "Failed to create task %s", task_name); */
+ /*  } else { */
+ /*    ESP_LOGI(TAG, "Motor task %s started on core %d", task_name, core_id); */
+ /* } */
 }
--- esp32-scara-robot/main/main.c
+++ esp32-scara-robot/main/main.c
@@ -3,35 +3,9 @@
 #include "motor.h"
 #include "motor_task.h"
 
-motor_t motor_x = {
-    .id = MOTOR_X,
-    .gpio_dir = GPIOXDIR,
-    .gpio_stp = GPIOXSTP,
-    .delay_us = 600,
-};
-
-motor_t motor_y = {
-    .id = MOTOR_Y,
-    .gpio_dir = GPIOYDIR,
-    .gpio_stp = GPIOYSTP,
-    .delay_us = 600,
-};
-
-motor_t motor_z = {
-    .id = MOTOR_Z,
-    .gpio_dir = GPIOZDIR,
-    .gpio_stp = GPIOZSTP,
-    .delay_us = 500,
-};
-
 void app_main(void) {
-  motor_initialization(&motor_x);
-  motor_initialization(&motor_y);
-  motor_initialization(&motor_z);
+  motor_init_all();
 
-  /* motor_task_start(&motor_x, "task_x", 5, 1); */
-  motor_task_start(&motor_y, "task_y", 5, 1);
-  motor_task_start(&motor_z, "task_z", 5, 0);
   /* while (1) { */
   /*   vTaskDelay(1000 / portTICK_PERIOD_MS / 3); */
   /* } */
