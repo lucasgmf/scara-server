b3c9d36758f1614305991d06d78c831bbe81ae95
lucasgmf <lucasgalvaomfreitas@gmail.com>
2025-04-30 16:54:27 +0100

    cleanup of motor_task files

--- esp32-scara-robot/components/control/motor_task.c
+++ esp32-scara-robot/components/control/motor_task.c
@@ -5,25 +5,20 @@
 #include "freertos/task.h"
 #include "motor.h"
 
-#define MOTOR_TASK_PERIOD_MS 1
-
+#define MOTOR_TASK_PERIOD_MS 10 // TODO: Maybe decrese this
 static const char *TAG = "MotorTask";
 
-// TODO: watchdog, stats, or timing metrics
 void motor_task(void *arg) {
   motor_t *motor = (motor_t *)arg;
   if (motor == NULL) {
-    ESP_LOGE("motor_task", "parameter motor is null, aborting.");
+    ESP_LOGE(TAG, "parameter motor is null, aborting.");
     vTaskDelete(NULL);
     return;
   }
-  /* TickType_t last_wake_time = xTaskGetTickCount(); */
-
-  while (true) {
-    ESP_LOGI("motor_task", "inside while, motor %d\n", motor->id);
+  TickType_t last_wake_time = xTaskGetTickCount();
+  while (1) {
     motor_update(motor);
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-    /* vTaskDelayUntil(&last_wake_time, pdMS_TO_TICKS(MOTOR_TASK_PERIOD_MS)); */
+    vTaskDelayUntil(&last_wake_time, pdMS_TO_TICKS(MOTOR_TASK_PERIOD_MS));
   }
 }
 
@@ -46,13 +41,3 @@ void motor_task_start(motor_t *motor, const char *task_name,
     ESP_LOGI(TAG, "Motor task %s started on core %d", task_name, core_id);
   }
 }
-//
-// --- 1. Update motors --- motor_test();
-
-// --- 2. Monitor system state (optional) ---
-// Example: check fault flags or encoder errors
-// if (motor_fault_detected()) {
-//     ESP_LOGE(TAG, "Motor fault detected!");
-// }
-
-// --- 3. Wait until next period ---
--- esp32-scara-robot/components/control/motor_task.h
+++ esp32-scara-robot/components/control/motor_task.h
@@ -4,7 +4,11 @@
 
 #include "motor.h"
 
+// TODO: watchdog, stats, or timing metrics
 void motor_task(void *arg);
 void motor_task_start(motor_t *motor, const char *task_name,
                       UBaseType_t priority, BaseType_t core_id);
+
+// Using a task for each motor mas the moviments not fluid...
+
 #endif // MOTOR_TASK_H
