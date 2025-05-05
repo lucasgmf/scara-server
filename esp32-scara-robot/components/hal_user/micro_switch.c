1097a40919ca43e16d3b66072e52ed1011c6bf4e
lucasgmf <lucasgalvaomfreitas@gmail.com>
2025-05-05 14:44:50 +0100

    added switch logic

--- esp32-scara-robot/components/hal_user/micro_switch.c
+++ esp32-scara-robot/components/hal_user/micro_switch.c
@@ -1,25 +1,20 @@
 #include "micro_switch.h"
 #include "driver/gpio.h"
 
-/* void button_init() { */
-/*   gpio_config_t io_conf = { */
-/*       .pin_bit_mask = (1ULL << BUTTON_GPIO), */
-/*       .mode = GPIO_MODE_INPUT, */
-/*       .pull_up_en = GPIO_PULLUP_ENABLE, // Enable pull-up */
-/*       .pull_down_en = GPIO_PULLDOWN_DISABLE, */
-/*       .intr_type = GPIO_INTR_DISABLE // No interrupts for now */
-/*   }; */
-/*   gpio_config(&io_conf); */
-/* } */
-
-void button_init_func() {
-  gpio_reset_pin(GPIO_NUM_4);
+void switch_init(switch_t *switch_) {
+  gpio_reset_pin(switch_->gpio_port);
   gpio_config_t io_conf = {
-      .pin_bit_mask = (1ULL << GPIO_NUM_4),
+      .pin_bit_mask = (1ULL << switch_->gpio_port),
       .mode = GPIO_MODE_INPUT,
-      .pull_up_en = GPIO_PULLUP_ENABLE, // Enable pull-up
+      .pull_up_en = GPIO_PULLUP_ENABLE,
       .pull_down_en = GPIO_PULLDOWN_DISABLE,
       .intr_type = GPIO_INTR_DISABLE // No interrupts for now
   };
   gpio_config(&io_conf);
+  return;
+}
+
+void update_switch_val(switch_t *switch_) {
+  switch_->value = gpio_get_level(switch_->gpio_port);
+  return;
 }
--- esp32-scara-robot/components/hal_user/micro_switch.h
+++ esp32-scara-robot/components/hal_user/micro_switch.h
@@ -1,6 +1,14 @@
 #ifndef MICRO_SWITCH_H
 #define MICRO_SWITCH_H
 
-void button_init_func();
+#include "driver/gpio.h"
+
+typedef struct {
+  gpio_num_t gpio_port;
+  bool value;
+} switch_t;
+
+void switch_init(switch_t *switch_);
+void update_switch_val(switch_t *switch_);
 
 #endif // MICRO_SWITCH_H
