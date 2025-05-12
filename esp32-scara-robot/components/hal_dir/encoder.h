9c63d8d7a1599c81bf207c4a60982da57b7377ea
lucasgmf <lucasgalvaomfreitas@gmail.com>
2025-05-12 16:38:47 +0100

    mag_encoder struct now includes needed pointers to i2c comm

--- esp32-scara-robot/components/hal_dir/encoder.h
+++ esp32-scara-robot/components/hal_dir/encoder.h
@@ -12,9 +12,12 @@ typedef struct {
   uint16_t raw_val;
   uint32_t offset;
   switch_t *cal_switch;
+  i2c_master_bus_handle_t *i2c_mst_config;
+  i2c_device_config_t *dev_cfg;
+  i2c_master_bus_handle_t *bus_handle;
+  i2c_master_dev_handle_t *as5600_dev_handle;
 } mag_encoder;
 
 uint16_t get_as5600_reading(i2c_master_dev_handle_t *as5600_dev_handle_t,
                             uint8_t msb_reg_angle);
-
 #endif // ENCODER_H
