6d4116415cc0aa08374c12d8f9e8d9dd01892dc8
lucasgmf <lucasgalvaomfreitas@gmail.com>
2025-05-12 17:15:07 +0100

    initialization of stp and dir is now separated

--- esp32-scara-robot/components/hal_dir/motor.c
+++ esp32-scara-robot/components/hal_dir/motor.c
@@ -1,18 +1,25 @@
 #include "motor.h"
 
-void init_motor(motor_t *motor_n) {
+void init_motor_dir(motor_t *motor_n) {
   if (motor_n == NULL) {
     ESP_LOGW("motor_initialization", "Pointer is null\n");
     return;
   }
 
   gpio_reset_pin(motor_n->gpio_dir);
-  gpio_reset_pin(motor_n->gpio_stp);
   gpio_set_direction(motor_n->gpio_dir, GPIO_MODE_OUTPUT);
-  gpio_set_direction(motor_n->gpio_stp, GPIO_MODE_OUTPUT);
 
   ESP_LOGW("motor_initialization", "Successfully initializated motor %d",
            motor_n->id);
   return;
 }
 
+
+void init_motor_stp(mcpwm_unit_t unit, mcpwm_timer_t timer, mcpwm_operator_t op,
+                    gpio_num_t gpio) {
+
+  mcpwm_io_signals_t signal =
+      (op == MCPWM_OPR_A) ? MCPWM0A + timer : MCPWM0B + timer;
+
+  mcpwm_gpio_init(unit, signal, gpio);
+}
--- esp32-scara-robot/components/hal_dir/motor.h
+++ esp32-scara-robot/components/hal_dir/motor.h
@@ -2,10 +2,10 @@
 #define MOTOR_H
 
 #include "driver/gpio.h"
+#include "driver/mcpwm.h"
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-
 
 typedef struct {
   int id;
@@ -18,7 +18,8 @@ typedef struct {
   int delay_us;
 } motor_t;
 
-
-void init_motor(motor_t *motor_n);
+void init_motor_dir(motor_t *motor_n);
+void init_motor_stp(mcpwm_unit_t unit, mcpwm_timer_t timer, mcpwm_operator_t op,
+                    gpio_num_t gpio);
 
 #endif // MOTOR_H
