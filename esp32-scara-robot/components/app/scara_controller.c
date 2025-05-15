7f4ce56c84bfecc5c8b60d281994372b52baf729
lucasgmf <lucasgalvaomfreitas@gmail.com>
2025-05-16 00:23:38 +0100

    testing 2 motors with new lib

--- esp32-scara-robot/components/app/scara_controller.c
+++ esp32-scara-robot/components/app/scara_controller.c
@@ -13,13 +13,13 @@ motor_t motor_x = {
     .move_ms = 0,
     .current_freq_hz = 200,
     .target_freq_hz = 0,
-    .speed_hz = 1000,
+    .speed_hz = 800,
 };
 
-motor_t motor2 = {
+motor_t motor_y = {
     .id = 1,
     .gpio_stp = GPIOYSTP,
-    .gpio_dir = GPIOYDIR, // optional
+    .gpio_dir = GPIOYDIR, 
     .step_count = 0,
     .mcpwm_unit = 1,
     .mcpwm_timer = 0,
@@ -27,15 +27,20 @@ motor_t motor2 = {
     .move_ms = 0,
     .current_freq_hz = 200,
     .target_freq_hz = 0,
-    .speed_hz = 1500,
+    .speed_hz = 800,
 };
 
 void init_scara() {
   motor_init_dir(&motor_x);
   motor_create_pwm(&motor_x);
 
+  motor_init_dir(&motor_y);
+  motor_create_pwm(&motor_y);
+
   xTaskCreatePinnedToCore(task_update_motor_pwm, "task_motor_pwm_x", 2048,
                           (void *)&motor_x, 5, NULL, 0);
+  xTaskCreatePinnedToCore(task_update_motor_pwm, "task_motor_pwm_y", 2048,
+                          (void *)&motor_y, 5, NULL, 0);
 
   ESP_LOGI(TAG, "Changing freq to 1000 hz");
   return;
@@ -44,24 +49,31 @@ void init_scara() {
 void loop_scara() {
   while (1) {
     gpio_set_level(motor_x.gpio_dir, 1);
+    gpio_set_level(motor_y.gpio_dir, 1);
     ESP_LOGI(TAG, "Changing freq to 1000 hz");
-    motor_x.target_freq_hz = 1000;
-    vTaskDelay(2500 / portTICK_PERIOD_MS);
+    motor_x.target_freq_hz = 400;
+    motor_y.target_freq_hz = 400;
+    vTaskDelay(500 / portTICK_PERIOD_MS);
 
     ESP_LOGI(TAG, "Changing freq to 0 hz");
     motor_x.target_freq_hz = 0;
-    vTaskDelay(2500 / portTICK_PERIOD_MS);
+    motor_y.target_freq_hz = 0;
+    vTaskDelay(500 / portTICK_PERIOD_MS);
     ESP_LOGI(TAG, "step_count: %d", motor_x.step_count);
 
     gpio_set_level(motor_x.gpio_dir, 0);
+    gpio_set_level(motor_y.gpio_dir, 0);
     ESP_LOGI(TAG, "Changing freq to 1000 hz");
-    motor_x.target_freq_hz = 1000;
-    vTaskDelay(2500 / portTICK_PERIOD_MS);
+    motor_x.target_freq_hz = 400;
+    motor_y.target_freq_hz = 400;
+    vTaskDelay(500 / portTICK_PERIOD_MS);
 
     ESP_LOGI(TAG, "Changing freq to 0 hz");
     motor_x.target_freq_hz = 0;
-    vTaskDelay(2500 / portTICK_PERIOD_MS);
+    motor_y.target_freq_hz = 0;
+    vTaskDelay(500 / portTICK_PERIOD_MS);
     ESP_LOGI(TAG, "step_count: %d", motor_x.step_count);
+    ESP_LOGI(TAG, "step_count: %d", motor_y.step_count);
   }
 
   /* vTaskDelay(1000 / portTICK_PERIOD_MS); */
