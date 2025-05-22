8ff4070613ae2f98841ec04ee44da6a49d95f587
lucasgmf <lucasgalvaomfreitas@gmail.com>
2025-05-23 00:36:07 +0100

    updating i2c structs

--- esp32-scara-robot/components/drivers/i2c_bus.h
+++ esp32-scara-robot/components/drivers/i2c_bus.h
@@ -7,5 +7,17 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/semphr.h"
 
+typedef struct {
+  i2c_master_bus_config_t *bus_cfg;
+  i2c_master_bus_handle_t *bus_handle;
+  SemaphoreHandle_t *i2c_mutex;
+  TickType_t timeout_ticks;
+} i2c_master_config_t;
+
+typedef struct {
+  i2c_device_config_t *dev_cfg;
+  i2c_master_dev_handle_t *dev_handle;
+} i2c_slave_bus_params;
+
 
 #endif // I2C_BUS_H
