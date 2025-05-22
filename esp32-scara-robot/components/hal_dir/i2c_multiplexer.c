d1f088ac0b653fe93b6495833a5f326f4daff091
lucasgmf <lucasgalvaomfreitas@gmail.com>
2025-05-23 00:34:49 +0100

    adding i2c_multiplexer logic

--- esp32-scara-robot/components/hal_dir/i2c_multiplexer.c
+++ esp32-scara-robot/components/hal_dir/i2c_multiplexer.c
@@ -1,2 +1,10 @@
 #include "i2c_multiplexer.h"
 
+esp_err_t tca_select_channel(uint8_t channel,
+                             i2c_master_dev_handle_t *tca_handle) {
+  if (channel > 7)
+    return ESP_ERR_INVALID_ARG;
+
+  uint8_t data = 1 << channel;
+  return i2c_master_transmit(*tca_handle, &data, 1, portMAX_DELAY);
+}
--- esp32-scara-robot/components/hal_dir/i2c_multiplexer.h
+++ esp32-scara-robot/components/hal_dir/i2c_multiplexer.h
@@ -1,7 +1,12 @@
 #ifndef I2C_MULTIPLEXER_H
 #define I2C_MULTIPLEXER_H
 
+#include "driver/i2c_master.h"
 #include "esp_err.h"
 #include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+
+esp_err_t tca_select_channel(uint8_t channel,
+                             i2c_master_dev_handle_t *tca_handle);
 
 #endif // I2C_MULTIPLEXER_H
