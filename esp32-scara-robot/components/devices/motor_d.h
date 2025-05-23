68a1db23a2c25572512bcc54538af836d0b5ab0d
lucasgmf <lucasgalvaomfreitas@gmail.com>
2025-05-23 15:58:56 +0100

    starting refactoring of motor logic

--- esp32-scara-robot/components/devices/motor_d.h
+++ esp32-scara-robot/components/devices/motor_d.h
@@ -6,15 +6,5 @@
 #include "motor.h"
 #include "pid.h"
 
-typedef struct {
-  /* encoder_t *encoder; */
-  motor_t *motor;
-  pid_controller_t *pid;
-  float target_position;    // Desired encoder position
-  float current_position;   // Read from encoder
-  float output_freq_hz;     // Output to motor (used as PWM freq)
-  float max_output_freq_hz; // Safety clamp
-  bool direction_reversed;  // Optional motor direction flip
-} motor_control_loop_t;
 
 #endif // MOTOR_D_H
--- esp32-scara-robot/components/hal_dir/motor.h
+++ esp32-scara-robot/components/hal_dir/motor.h
@@ -1,32 +1,5 @@
 #ifndef MOTOR_H
 #define MOTOR_H
 
-#include "driver/gpio.h"
-#include "driver/mcpwm_prelude.h"
-#include "esp_timer.h"
-
-typedef struct {
-  int id;
-  gpio_num_t gpio_stp;
-  gpio_num_t gpio_dir;
-  int step_count;
-  int mcpwm_unit;
-  int mcpwm_timer;
-  int mcpwm_opr;
-  int move_ms;
-  float current_freq_hz;
-  int target_freq_hz;
-  float speed_hz; // Hz per second
-  mcpwm_timer_handle_t pwm_timer;
-  mcpwm_oper_handle_t pwm_oper;
-  mcpwm_cmpr_handle_t pwm_comparator;
-  mcpwm_gen_handle_t pwm_generator;
-  esp_timer_handle_t update_timer;
-} motor_t;
-
-void motor_init_dir(motor_t *motor_n);
-void motor_create_pwm(motor_t *motor);
-void motor_set_frequency(motor_t *motor, int target_freq_hz);
-void motor_delete_pwm(motor_t *motor);
 
 #endif // MOTOR_H
